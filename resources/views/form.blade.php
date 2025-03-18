1224a4a0a803204a6457f2da40ce36559fddfc83
ThiagoTDS <tdsdias.tc3@gmail.com>
2025-03-18 10:18:01 -0300

    finalizada a view cadastrar veiculos

--- resources/views/form.blade.php
+++ resources/views/form.blade.php
@@ -81,19 +81,104 @@
                         @enderror
                     </div>
                      <div class="form-group col-sm-12 col-md-6 mb-3">
-                        <label for="kilometers">Kilometragem</label>
+                        <label for="type">Tipo de veículo</label>
+                        <select class="form-select" name="type" id="type">
+                            <option value="">Selecione uma opção</option>
+                            <option value=""
+                            @if(isset($item)&& $item->type == "Novo" || isset($item) && $item->type == old('type')) selected @endif
+                            >Novo</option>
+                            <option value=""
+                            @if(isset($item)&& $item->type == "Usado" || isset($item) && $item->type == old('type')) selected @endif
+                            >Usado</option>
+                        </select>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="form-group col-sm-12 col-md-4 mb-3">
+                        <label for="price">Preço</label>
                         <input 
                         class="form-control"
-                        id="kilometers" 
-                        name="kilometers"
-                        value="{{isset($item) ? $item->kilometers: old('kilometers') }}"
+                        id="price" 
+                        name="price"
+                        value="{{isset($item) ? $item->price: old('price') }}"
                         type="text"
                         autocomplete="off">
-                        @error('brand')
+                        @error('price')
+                        <div class="alert alert-danger mt-3"> {{$message}}</div>
+                        @enderror
+                    </div>
+                     <div class="form-group col-sm-12 col-md-8 mb-3">
+                        <label for="description">Descrição</label>
+                        <input 
+                        class="form-control"
+                        id="description" 
+                        name="description"
+                        value="{{isset($item) ? $item->description: old('description') }}"
+                        type="text"
+                        autocomplete="off">
+                        @error('description')
                         <div class="alert alert-danger mt-3"> {{$message}}</div>
                         @enderror
                     </div>
                 </div>
+                <div class="row">
+                    <div class="form-group col-sm-12 mb-3">
+                        <label for="image">Foto</label>
+                        <input 
+                        type="file"
+                        class="form-control"
+                        id="image"
+                        name="image"
+                        value="{{isset($item) ? $item->image : old('image')}}"
+                        autocomplete="off">
+
+                        @error('image')
+                        <div class="alert alert-danger mt-3">{{ $message }}</div>
+                        @enderror
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="form-group col-sm-12 col-md-4 mb-3">
+                        <label for="contact_name">Nome do vendedor</label>
+                        <input 
+                        class="form-control"
+                        id="contact_name" 
+                        name="contact_name"
+                        value="{{isset($item) ? $item->contact_name: old('contact_name') }}"
+                        type="text"
+                        autocomplete="off">
+                        @error('contact_name')
+                        <div class="alert alert-danger mt-3"> {{$message}}</div>
+                        @enderror
+                    </div>
+                     <div class="form-group col-sm-12 col-md-4 mb-3">
+                        <label for="contact_phone">Telefone do vendedor</label>
+                        <input 
+                        class="form-control"
+                        id="contact_phone" 
+                        name="contact_phone"
+                        value="{{isset($item) ? $item->contact_phone: old('contact_phone') }}"
+                        type="text"
+                        autocomplete="off">
+                        @error('contact_phone')
+                        <div class="alert alert-danger mt-3"> {{$message}}</div>
+                        @enderror
+                    </div>
+                    <div class="form-group col-sm-12 col-md-4 mb-3">
+                        <label for="contact_mail">E-mail do vendedor</label>
+                        <input 
+                        class="form-control"
+                        id="contact_mail" 
+                        name="contact_mail"
+                        value="{{isset($item) ? $item->contact_mail: old('contact_mail') }}"
+                        type="text"
+                        autocomplete="off">
+                        @error('contact_mail')
+                        <div class="alert alert-danger mt-3"> {{$message}}</div>
+                        @enderror
+                    </div>
+                </div>
+                <button class="btn btn-success">Salvar</button>
             </form>
         </div>
     </div>
